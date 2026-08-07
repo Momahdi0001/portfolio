1a18da6e091bf06b825232f6744583b575e536e9
momahdi <momahdi0010@gmail.com>
2026-08-07 10:05:25 +0330

    Add files via upload

---
+++ Data_Analyst/RFM_Analysis_&_Customer_Segmentation/02.RFM_Calculation_use_JOIN.sql
@@ -0,0 +1,69 @@
+﻿/*
+شناسایی داده ها و درک  جدول
+*/
+
+SELECT SalesOrderID as [شناسه منحصر به فرد سفارش]
+	,OrderDate as [تاریخ ثبت سفارش]
+	,CustomerID as [شناسه مشتری]
+	,TotalDue as [مبلغ کل سفارش]
+FROM Sales.SalesOrderHeader
+
+
+SELECT SalesOrderID as[شناسه منحصر به فرد سفارش]
+	,SalesOrderDetailID as [شناسه منحصر به فرد هر آیتم]
+	,OrderQty as [اعداد محصولات سفارش داده شده]
+	,ProductID as [شناسه محصول]
+	,UnitPrice as [قیمت هر محصول]
+	,LineTotal as [مبلغ کل آیتم]
+FROM Sales.SalesOrderDetail
+
+/*
+مقدار Recency
+به یک “تاریخ امروز” برای مقایسه نیاز دارد
+به جای استفاده از 
+GETDATE()
+از تاریخ آخرین سفارش ثبت‌شده در پایگاه داده به عنوان نقطه مرجع استفاده شده است
+  به دلیل اینکه تاریخ هر روز تغییر میکند و ما از مرجعی ثابت استفاده کرده ایم
+*/
+DECLARE @Today DATE = (SELECT MAX(OrderDate) 
+					  FROM Sales.SalesOrderHeader
+					  );
+
+/*
+یک جدول CTE 
+ساخته شده تا مقادیر 
+R,F,M
+را نمایش دهد
+برای محاسبه تازگی اخرین خرید هر مشتری را از مرجع تاریخ که تعریف کردیم کسر کرده
+تا خروجی مشخص کند مشتری اخرین خریدش چند روز گذشته بوده است
+هر چه تازگی کمتر باشه مشتری فعال تر است
+برای محاسبه اینکه مشتری چند بار سفارش ثبت کرده است
+پس میگوییم : برای هر مشتری بشمار تعداد شناسه های سفارش فروش اش را
+و برای اینکه هر مشتری چندین اقلام در فاکتورش دارد باید از
+DISTINCT
+استفاده کنیم تا تعداد سفارش های یکتا را بشماریم
+محسابه ارزش پولی از جدول SOD
+SUM(LineTotal)
+جمع کل خرید های مشتری را محاسبه میکند
+که مبلع خالص خرید را مشخص میکند و هزینه بسته بندی و... را در نظر نمیگرد
+از CAST 
+استفاده شده تا خروجی را با دو رفم اعشار گرد شود
+*/
+
+WITH RFM_Base AS (
+    SELECT SOH.CustomerID
+        ,DATEDIFF(day, MAX(SOH.OrderDate), @Today) AS [Recency]
+        ,COUNT(DISTINCT SOH.SalesOrderID) AS [Frequency]
+        ,CAST(SUM(SOD.LineTotal) AS DECIMAL(18, 2)) AS [Monetary]
+        
+    FROM Sales.SalesOrderHeader SOH
+    JOIN 
+		Sales.SalesOrderDetail SOD 
+			ON SOH.SalesOrderID = SOD.SalesOrderID
+    GROUP BY 
+        SOH.CustomerID
+)
+SELECT * 
+FROM RFM_Base
+ORDER BY CustomerID ASC;
+
---
+++ Data_Analyst/RFM_Analysis_&_Customer_Segmentation/05.RFM_Segmentation.sql
@@ -0,0 +1,80 @@
+﻿/*
+خوشه بندی مشتریان به این صورت خواهد بود
+ارزش بالا ===4
+ارزش متوسط===3
+ارزش پایین===2
+در معرض خطر===1
+*/
+DECLARE @Today DATE = (
+						SELECT MAX(OrderDate)
+						FROM Sales.SalesOrderHeader
+				);
+WITH RFM_BASE 
+	AS (
+		SELECT CustomerID
+			,DATEDIFF(DAY,MAX(OrderDate),@Today) as Recency
+			,COUNT(SalesOrderID) as Frequency
+			,CAST(SUM(SubTotal) AS DECIMAL(18, 2)) AS Monetary
+		FROM Sales.SalesOrderHeader
+		GROUP BY CustomerID
+	),
+
+	RFM_SCORE
+	AS (
+		SELECT CustomerID
+			,Recency
+			,Frequency
+			,Monetary
+
+			,NTILE(4) OVER(ORDER BY Recency DESC) AS Recency_Score
+			,NTILE(4) OVER (ORDER BY Frequency ASC) AS Frequency_Score
+			,NTILE(4) OVER (ORDER BY Monetary ASC) AS Monetary_Score
+		FROM RFM_BASE
+	),
+
+RFM_Segmentation AS (
+    SELECT 
+        CustomerID
+        ,Recency
+        ,Frequency
+        ,Monetary
+        ,Recency_Score
+        ,Frequency_Score
+        ,Monetary_Score
+        ,CONCAT(Recency_Score, Frequency_Score, Monetary_Score) AS RFM_Cell,
+        
+        CASE 
+            -- ارزش بالا: مشتریانی که اخیراً خرید کرده‌اند، زیاد خرید می‌کنند و پول زیادی خرج می‌کنند
+            WHEN Recency_Score >= 3 AND Frequency_Score >= 3 AND Monetary_Score >= 3 THEN N'High Value (ارزش بالا)'
+            
+            -- در معرض خطر: مشتریانی که قبلاً زیاد خرید می‌کردند اما مدت‌هاست بازنگشته‌اند
+            WHEN Recency_Score <= 2 AND Frequency_Score >= 3 THEN N'At Risk (در معرض خطر)'
+            
+            -- ارزش پایین: مشتریانی که خیلی وقت پیش خرید کرده‌اند و تعداد و مبلغ خریدشان هم کم بوده است
+            WHEN Recency_Score <= 2 AND Frequency_Score <= 2 AND Monetary_Score <= 2 THEN N'Low Value (ارزش پایین)'
+            
+            -- ارزش متوسط: سایر مشتریانی که در دسته‌های افراطی بالا قرار نمی‌گیرند
+            ELSE N'Medium Value (ارزش متوسط)'
+        END AS Customer_Segment
+    FROM 
+        RFM_SCORE
+)
+
+-- 5. نمایش نهایی داده‌ها (با قابلیت JOIN به جداول مشتری برای اطلاعات دموگرافیک در صورت نیاز)
+SELECT 
+    S.CustomerID,
+    P.FirstName,
+    P.LastName,
+    S.Customer_Segment,
+    S.RFM_Cell,
+    S.Recency_Score,
+    S.Frequency_Score,
+    S.Monetary_Score
+FROM 
+    RFM_Segmentation AS S
+    -- اضافه کردن اطلاعات شخصی مشتریان 
+    LEFT JOIN Sales.Customer AS C ON S.CustomerID = C.CustomerID
+    LEFT JOIN Person.Person AS P ON C.PersonID = P.BusinessEntityID
+ORDER BY 
+    S.Customer_Segment, S.RFM_Cell DESC;
+
